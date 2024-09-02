018ac0e59e3deecc39cb754105849773c37524b8
flexibility2 <729406307@qq.com>
2024-09-02 19:09:27 +0800

    Style the activate link in Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,28 +1,47 @@
-import Link from 'next/link'
-import React from 'react'
-import { AiFillBug } from 'react-icons/ai'
+'use client';
+import classnames from "classnames";
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
-
-const Links = [{
-    herf: '/', label: 'Dashborad' 
-}, {
-    herf: '/issues', label: 'Issues'
-}]
+  const currentPath = usePathname();
+  const Links = [
+    {
+      herf: "/",
+      label: "Dashborad",
+    },
+    {
+      herf: "/issues",
+      label: "issues",
+    },
+  ];
 
   return (
     <>
-    <nav className='flex items-center space-x-6 border-b mb-5 px-5 h-14'>
-        <Link href={'/'}><AiFillBug></AiFillBug> </Link>
-        <ul className='flex items-center space-x-6'>
-            {Links.map((link, index) => (
-                    <Link href={link.herf} key={index} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>
-            ))}
+      <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
+        <Link href={"/"}>
+          <AiFillBug></AiFillBug>
+        </Link>
+        <ul className="flex items-center space-x-6">
+          {Links.map((link, index) => (
+            <Link
+              href={link.herf}
+              key={index}
+              className={classnames({
+                'text-zinc-900': link.herf === currentPath,
+                'text-zinc-500': link.herf !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+              })} 
+            >
+              {link.label}
+            </Link>
+          ))}
         </ul>
-
-    </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
