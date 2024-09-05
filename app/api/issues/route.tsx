b09d7ea98ed01123af4dc888f4f9b2d3f7e27390
flexibility2 <729406307@qq.com>
2024-09-05 21:48:32 +0800

    handle error

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -3,16 +3,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-})
+  title: z.string().min(1).max(100),
+  description: z.string().min(1).max(1000),
+});
 
 export async function POST(request: NextRequest) {
-    const data = await request.json();
-    const result = createSchema.safeParse(data);
-    if (!result.success) {
-        return NextResponse.json({ message: 'Invalid data', errors: result.error.errors }, { status: 400 });
-    }
-    const createResult =  await prisma.issue.create({data: { title: data.title, description: data.description }});
-    return NextResponse.json(createResult, {status: 201});
+  const data = await request.json();
+  const result = createSchema.safeParse(data);
+  if (!result.success) {
+    return NextResponse.json(result.error.format(), { status: 400 });
   }
+  const createResult = await prisma.issue.create({
+    data: { title: data.title, description: data.description },
+  });
+  return NextResponse.json(createResult, { status: 201 });
+}
