9aa6116d55da7b486b9353e7e50a2d317d324f1f
flexibility2 <729406307@qq.com>
2024-09-08 13:55:16 +0800

    Add more loading

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,13 @@
+import { Box } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton height={"20rem"}></Skeleton>
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
