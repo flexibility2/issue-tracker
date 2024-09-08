96632f236d24f3d803569ddfd4c4e169e535efce
flexibility2 <729406307@qq.com>
2024-09-08 13:45:39 +0800

    style link

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
 import { Status } from "@prisma/client";
 import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import Link from "../components/Link";
 import React from "react";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
@@ -29,7 +29,7 @@ const IssuePage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title} </Link>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
