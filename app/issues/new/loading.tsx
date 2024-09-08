0a02e59ac7b621ddfb46eb928d64c327dd65b5b3
flexibility2 <729406307@qq.com>
2024-09-08 17:56:03 +0800

    organize import statements

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@radix-ui/themes";
 import React from "react";
-import Skeleton from "react-loading-skeleton";
+import { Skeleton } from "@/app/components";
 import delay from "delay";
 const LoadingNewIssuePage = () => {
   //   await delay(2000);
