463d81dbfb6d26c68befae04650ea36ef47ec9a1
flexibility2 <729406307@qq.com>
2024-09-22 20:41:16 +0800

    configure google provider but not work

---
+++ app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+const hander = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+});
+export { hander as GET, hander as POST };
