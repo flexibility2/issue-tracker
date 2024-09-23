7d85cc6ef117dd46b3d0fb7be507f33d94e820be
flexibility2 <729406307@qq.com>
2024-09-23 18:13:05 +0800

    Add drop-down menu

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,15 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const Navbar = () => {
   const currentPath = usePathname();
@@ -20,6 +28,7 @@ const Navbar = () => {
     },
   ];
   const { status, data: session } = useSession();
+  console.log("session: ", session?.user?.image);
 
   return (
     <>
@@ -48,10 +57,30 @@ const Navbar = () => {
             </Flex>
             <Box>
               {status === "authenticated" && (
-                <Link href={"/api/auth/signout"}>Log out</Link>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <Button variant="outline">
+                      <Avatar
+                        src={session.user!.image!}
+                        fallback="?"
+                        size="2"
+                        radius="full"
+                        className="cursor-pointer"
+                      />
+                    </Button>
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                      <Text size="2">{session.user!.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                      <Link href="/api/auth/signout">Log out</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
               )}
               {status === "unauthenticated" && (
-                <Link href={"/api/auth/signin"}>Log in</Link>
+                <Link href="/api/auth/signin">Login</Link>
               )}
             </Box>
           </Flex>
