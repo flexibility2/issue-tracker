9fa63b1113e029f5927fd0e2d6e2208e38a3b4b8
flexibility2 <729406307@qq.com>
2024-11-07 10:11:33 +0800

    Refactor AssigneeSelect

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -12,12 +12,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     data: users,
     error,
     isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, //60s,
-    retry: 3,
-  });
+  } = useUser();
 
   if (isLoading) {
     return <Skeleton></Skeleton>;
@@ -25,19 +20,23 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 
   if (error) return null;
 
+
+  const assignIssue = (userId: string) =>{
+    axios
+    .patch("/api/issues/x" + issue.id, {
+      assignedToUserId: userId || null,
+    })
+    .catch(() => {
+      toast.error("Changes could not be saved.");
+    });
+  }
+
+
   return (
     <>
       <Select.Root
         defaultValue={issue?.assignedToUserId || ""}
-        onValueChange={(userId) => {
-          axios
-            .patch("/api/issues/x" + issue.id, {
-              assignedToUserId: userId || null,
-            })
-            .catch(() => {
-              toast.error("Changes could not be saved.");
-            });
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -58,3 +57,11 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 };
 
 export default AssigneeSelect;
+
+const useUser = () => 
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60s,
+    retry: 3,
+  });
