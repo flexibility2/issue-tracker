f8a35ed3eee9ac219aca6a02bb1edc576e329d45
flexibility2 <729406307@qq.com>
2024-12-08 14:43:06 +0800

    issue summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return <LatestIssues></LatestIssues>;
+// export default function Home({
+//   searchParams,
+// }: {
+//   searchParams: { page: string };
+// }) {
+//   return <LatestIssues></LatestIssues>;
+// }
+
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "DONE" },
+  });
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
